f4d628104ebe64a95a58cb7a981e361c9cc14756
ALX <alexmonac13@gmail.com>
2023-08-23 00:55:19 +0200

    Add Box panel color input states

--- src/layouts/BoxPanel/BoxColorPicker.jsx
+++ src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,3 +1,35 @@
-export default function BoxColorPicker() {
-  return <div>BoxColorPicker</div>;
+import { useDispatch } from "react-redux";
+import { updateBoxValue } from "../../features/boxProperties";
+
+export default function BoxColorPicker({ inputData }) {
+  const dispatch = useDispatch();
+
+  function handleInputs(e) {
+    dispatch(
+      updateBoxValue({
+        inputNumber: inputData.inputNumber,
+        value: e.target.value,
+      })
+    );
+  }
+
+  return (
+    <div className="mt-3">
+      <p> {inputData.name} </p>
+      <div className="flex mt-2">
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="flex-grow border py-1 px-2 focus:outline-1 outline-gray-400"
+          type="text"
+        />
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="cursor-pointer h-[40px]"
+          type="color"
+        />
+      </div>
+    </div>
+  );
 }

82a4faa3a0a73e50699cdeaf5d0688e974fabaab
ALX <alexmonac13@gmail.com>
2023-08-23 00:04:48 +0200

    Init Redux Store & reducers

---
+++ src/features/boxProperties.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    inputNumber: 1,
+    name: "Border radius",
+    value: 25,
+    type: "range",
+    minMax: [0, 250],
+    slice: "boxProperties",
+  },
+  {
+    inputNumber: 2,
+    name: "Height",
+    value: 250,
+    type: "range",
+    minMax: [0, 500],
+    slice: "boxProperties",
+  },
+  {
+    inputNumber: 3,
+    name: "Width",
+    value: 250,
+    type: "range",
+    minMax: [0, 500],
+    slice: "boxProperties",
+  },
+  {
+    inputNumber: 4,
+    name: "Background color",
+    value: "#fff",
+    type: "color",
+    slice: "boxProperties",
+  },
+];
+
+export const boxSlice = createSlice({
+  name: "boxProperties",
+  initialState,
+  reducers: {
+    updateBoxValue: (state, action) => {},
+  },
+});
+
+export const { updateBoxValue } = boxSlice.actions;
+export default boxSlice.reducer;

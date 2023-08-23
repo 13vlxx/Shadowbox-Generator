2edd4acdb09ff02b70534e4a0673322d74a7c14d
ALX <alexmonac13@gmail.com>
2023-08-23 15:24:21 +0200

    Fix responsive UI

--- src/layouts/LeftContainer.jsx
+++ src/layouts/LeftContainer.jsx
@@ -11,7 +11,7 @@ export default function LeftContainer() {
   ];
 
   return (
-    <div className="relative mt-20 w-[600px] h-[550px] border rounded-b rounded-tr border-gray-300 shadow-xl bg-gray-50 md:mt-0">
+    <div className="relative mt-32 w-[600px] h-[550px] border rounded-b rounded-tr border-gray-300 shadow-xl bg-gray-50 md:mt-0">
       <div className="flex absolute -translate-y-full -left-[1px]">
         {tabsList.map((tab, index) => (
           <button

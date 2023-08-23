02bfb6da42dec4ecbace73335a0055f488096d99
ALX <alexmonac13@gmail.com>
2023-08-23 15:26:10 +0200

    Fix header positioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="fixed w-full z-1 p-4 border-b-2 border-gray-200 bg-gray-50">
+    <header className="absolute top-0 w-full z-1 p-4 border-b-2 border-gray-200 bg-gray-50">
       <h1 className="text-xl text-center uppercase font-semibold">BoxShadow Generator</h1>
     </header>
   );

cc4d2f908c43e6fbbdaada66e07bce59d5fa69a7
ALX <alexmonac13@gmail.com>
2023-08-23 03:12:51 +0200

    Fix bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="p-2 border-t-2 border-gray-200">
+    <footer className="p-2 border-t-2 border-gray-200 bg-gray-50">
       <p className="text-sm center text-center uppercase">13vlxx - All right reserved ©</p>
     </footer>
   );
